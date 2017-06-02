3e89849090f5def300359cc323af1bb065b25672
João Lino Costa <projectomv@gmail.com>
2017-06-02 13:21:48 +0100

    Add files via upload

--- UMeR.java
+++ UMeR.java
@@ -1,30 +1,38 @@
 import java.util.*;
 import java.lang.Math;
+import java.io.ObjectOutputStream;
+import java.io.ObjectInputStream;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Serializable;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.util.Iterator;
 
-
-public class UMeR
+public class UMeR implements Serializable
 {
-    private HashMap<Integer, Motorista> hashMapMotoristas;
-    private HashMap<String, Cliente> hashMapClientes;
-    private HashMap<Integer, Veiculo> hashMapVeiculos;
-    private HashMap<String, Empresa> hashMapEmpresas;
-    //git
+    private Map<String, Motorista> hashMapMotoristas;
+    private Map<String, Cliente> hashMapClientes;
+    private Map<Integer, Veiculo> hashMapVeiculos;
+    private Map<String, Empresa> hashMapEmpresas;
     
     public UMeR()
     {
-        this.hashMapMotoristas = new HashMap<Integer, Motorista>();
+        this.hashMapMotoristas = new HashMap<String, Motorista>();
         this.hashMapClientes = new HashMap<String, Cliente>();
         this.hashMapVeiculos = new HashMap<Integer, Veiculo>();
         this.hashMapEmpresas = new HashMap<String, Empresa>();
     }
     
     
-    public UMeR(HashMap<Integer, Motorista> pHashMapMotoristas, 
-                HashMap<String, Cliente> pHashMapClientes, 
-                HashMap<Integer, Veiculo> hashMapVeiculos, 
-                HashMap<String, Empresa> pHashMapEmpresas)
+    public UMeR(Map<String, Motorista> pHashMapMotoristas, 
+                Map<String, Cliente> pHashMapClientes, 
+                Map<Integer, Veiculo> hashMapVeiculos, 
+                Map<String, Empresa> pHashMapEmpresas)
     {
-        this.hashMapMotoristas = new HashMap<Integer, Motorista>(hashMapMotoristas);
+        this.hashMapMotoristas = new HashMap<String, Motorista>(hashMapMotoristas);
         this.hashMapClientes = new HashMap<String, Cliente>(hashMapClientes);
         this.hashMapVeiculos = new HashMap<Integer, Veiculo>(hashMapVeiculos);
         this.hashMapEmpresas = new HashMap<String, Empresa>(pHashMapEmpresas);
@@ -40,11 +48,28 @@ public class UMeR
     }
     
     
-    public HashMap<Integer, Motorista> getHashMapMotoristas()   { return this.hashMapMotoristas;   }
-    public HashMap<String, Cliente> getHashMapClientes()        { return this.hashMapClientes;     }
-    public HashMap<Integer, Veiculo> getHashMapVeiculos()       { return this.hashMapVeiculos;     }
-    public HashMap<String, Empresa> getHashMapEmpresas()        { return this.hashMapEmpresas;     }
+    public Map<String, Motorista> getHashMapMotoristas()   { return this.hashMapMotoristas;   }
+    public Map<String, Cliente> getHashMapClientes()        { return this.hashMapClientes;     }
+    public Map<Integer, Veiculo> getHashMapVeiculos()       { return this.hashMapVeiculos;     }
+    public Map<String, Empresa> getHashMapEmpresas()        { return this.hashMapEmpresas;     }
+    
+    public void setHashMapMotoristas(Map<String,Motorista> m) {this.hashMapMotoristas = m;}
+    public void setHashMapClientes(Map<String,Cliente> c) {this.hashMapClientes = c;}
+    public void setHashMapVeiculos(Map<Integer,Veiculo> v) {this.hashMapVeiculos = v;}
+    public void setHashMapEmpresas(Map<String,Empresa> e) {this.hashMapEmpresas = e;}
       
+    
+    public String toString()
+    {
+        StringBuilder s = new StringBuilder();
+        Iterator <Map.Entry<String,Cliente>>  it = this.hashMapClientes.entrySet().iterator();
+        
+        while(it.hasNext())
+        {
+           s.append(it.next().getValue().toString());
+        }
+        return s.toString();
+    }
     
     public UMeR clone() 
     { 
@@ -54,16 +79,16 @@ public class UMeR
     
     public void insereMotorista(Motorista pMotorista) 
     { 
-        this.hashMapMotoristas.put(pMotorista.getIdTaxi(), pMotorista); 
+        this.hashMapMotoristas.put(pMotorista.getEmail(), pMotorista.clone()); 
     }
         
-    public Motorista procuraMotoristaPorKey(int pIdTaxi)
+    public Motorista procuraMotoristaPorKey(String email)
     {
         Motorista m = null;
         
-        if (this.hashMapMotoristas.containsKey(pIdTaxi)) {
+        if (this.hashMapMotoristas.containsKey(email)) {
             m = new Motorista();
-            m = this.hashMapMotoristas.get(pIdTaxi);
+            m = this.hashMapMotoristas.get(email);
         }
         
         return m;
@@ -72,7 +97,7 @@ public class UMeR
     
     public void insereCliente(Cliente pCliente) 
     {
-        this.hashMapClientes.put(pCliente.getEmail(), pCliente);
+        this.hashMapClientes.put(pCliente.getEmail(), pCliente.clone());
     }
     
     public boolean procuraCliente(Cliente pCliente)
@@ -80,32 +105,9 @@ public class UMeR
         return this.hashMapClientes.containsKey(pCliente.getEmail());
     }
     
-    public void chamarUmTaxiPorId(Cliente pCliente, double pX, double pY, int pIdTaxi) throws MotoristaExcepcoes
+    public void insereVeiculo(Veiculo pVeiculo) 
     {
-        Motorista m = new Motorista();
-        Viagem v = new Viagem();
-        
-        if (procuraCliente(pCliente)) {
-            pCliente.setX(pX);
-            pCliente.setY(pY);
-        
-            m = procuraMotoristaPorKey(pIdTaxi);
-            if (m != null) {
-                if (!m.getEstaDisponivel()) {
-                    throw new MotoristaExcepcoes ("O motorista nao esta disponivel!");
-                } else {
-                    v.setDistanciaTaxiCliente(v.calcularUmaDistancia(pCliente.getX(), pCliente.getY(), m.getX(), m.getY()));
-                    System.out.println("Introduza as coordenadas de destino: ");
-                    Scanner teclado = new Scanner(System.in);
-                }
-            }
-            
-        }
-    }
-    
-    public void insereVeiculo(Veiculo pVeiculo)
-    {
-        this.hashMapVeiculos.put(pVeiculo.getFactorDeFiabilidade(), pVeiculo);
+        this.hashMapVeiculos.put(pVeiculo.getFactorDeFiabilidade(), pVeiculo.clone());
     }
     
     public boolean procuraVeiculo(Veiculo pVeiculo)
@@ -116,11 +118,76 @@ public class UMeR
     
     public void insereEmpresa(Empresa pEmpresa)
     {
-        this.hashMapEmpresas.put(pEmpresa.getNome(), pEmpresa);
+        this.hashMapEmpresas.put(pEmpresa.getNome(), pEmpresa.clone());
     }
     
     public boolean procuraEmpresa(Empresa pEmpresa)
     {
         return this.hashMapEmpresas.containsKey(pEmpresa);
-    }    
+    }
+    
+   public void gravar(String file) throws IOException
+   {
+       ObjectOutputStream oss = new ObjectOutputStream(new FileOutputStream(file));
+       oss.writeObject(this);
+       oss.flush();
+       oss.close();
+   }
+   
+   public UMeR ler(String file) throws IOException ,ClassNotFoundException
+   {
+       ObjectInputStream in = new ObjectInputStream(new FileInputStream(file));
+       UMeR res = (UMeR) in.readObject();
+       in.close();
+       return res;
+   }  
+   
+   public double facturadoPorMotoristaEntre(Motorista m,LocalDateTime date1,LocalDateTime date2)
+   {
+       Motorista m1 = this.hashMapMotoristas.get(m.getEmail());
+       List<Viagem> l = m1.getHistoricoDeViagens();
+       l.removeIf((Viagem v2) -> v2.getData().isBefore(date1));
+       l.removeIf((Viagem v3) -> v3.getData().isAfter(date2));
+       
+       double total=0;
+       Iterator<Viagem> it = l.iterator();
+       while(it.hasNext())
+       {
+           Viagem tmp=it.next();
+           total+=tmp.getDistanciaDaViagem()*tmp.getPrecoDaViagem();
+       }
+       return total;
+   }
+   
+   public double facturadoPorTaxiEntre(Veiculo v,LocalDateTime date1,LocalDateTime date2)
+   {
+       Veiculo v1 = this.hashMapVeiculos.get(v.getId());
+       List<Viagem> l = v1.getHistoricoDeViagens();
+       l.removeIf((Viagem v2) -> v2.getData().isBefore(date1));
+       l.removeIf((Viagem v3) -> v3.getData().isAfter(date2));
+       
+       double total=0;
+       Iterator<Viagem> it = l.iterator();
+       while(it.hasNext())
+       {
+           Viagem tmp=it.next();
+           total+=tmp.getDistanciaDaViagem()*tmp.getPrecoDaViagem();
+       }
+       return total;
+   }
+   
+   public double facturadoPorEmpresaEntre(Empresa e,LocalDateTime date1,LocalDateTime date2)
+   {
+       Empresa e1 = this.hashMapEmpresas.get(e.getNome());
+       Collection <Motorista> l =e1.getHashMapMotoristas().values();
+    
+       
+       double total=0;
+       Iterator<Motorista> it = l.iterator();
+       while(it.hasNext())
+       {
+           total+=facturadoPorMotoristaEntre(it.next(),date1,date2);
+       }
+       return total;
+   }
 }
--- Veiculo.java
+++ Veiculo.java
@@ -1,53 +1,89 @@
 import java.util.*;
+import java.io.Serializable;
 
 
 
-public abstract class Veiculo
+public abstract class Veiculo implements Serializable
 {
+    private int id;
+    private String marca;
+    private String modelo;
     private double velocidadeMedia;
     private double precoBase;
     private int factorDeFiabilidade;
     private int capacidade;
-    private ArrayList<Viagem> historicoDeViagens;
+    private double totalKms;
+    private List<Viagem> historicoDeViagens;
+    private List<Viagem> filaDeEspera;
    
     
     
     public Veiculo()
     {
+        this.id=-1;
+        this.marca = "";
+        this.modelo = "";
         this.velocidadeMedia = 0.00;
         this.precoBase = 0.00;
         this.factorDeFiabilidade = 0;
         this.capacidade = 0;
+        this.totalKms=0;
         this.historicoDeViagens = new ArrayList<Viagem>();
+        this.filaDeEspera = new ArrayList<Viagem>();
     }
     
     
-    public Veiculo(double pVelocidadeMedia, double pPrecoBase ,int pFactorDeFiabilidade, int pCapacidade, ArrayList<Viagem> pHistoricoDeViagens)
+    public Veiculo(int id1,String marca1,String modelo1,double pVelocidadeMedia, double pPrecoBase ,int pFactorDeFiabilidade, int pCapacidade, List<Viagem> l, List<Viagem> fe)
     {
+        this.id=id1;
+        this.marca=marca1;
+        this.modelo=modelo1;
         this.velocidadeMedia = pVelocidadeMedia;
         this.precoBase = pPrecoBase;
         this.factorDeFiabilidade = pFactorDeFiabilidade;
         this.capacidade = pCapacidade;
-        this.historicoDeViagens = new ArrayList<Viagem>(pHistoricoDeViagens);
+        this.totalKms=0;
+        this.historicoDeViagens = new ArrayList<Viagem>(l);
+        this.filaDeEspera = new ArrayList<Viagem>(fe);
     }
     
     
     public Veiculo(Veiculo v)
     {
+        this.id=v.getId();
+        this.marca=v.getMarca();
+        this.modelo=v.getModelo();
         this.velocidadeMedia = v.getVelocidadeMedia();
         this.precoBase = v.getPrecoBase();
         this.factorDeFiabilidade = v.getFactorDeFiabilidade();
         this.capacidade = v.getCapacidade();
+        this.totalKms = v.gettotalKms();
         this.historicoDeViagens = v.getHistoricoDeViagens();
+        this.filaDeEspera = v.getFilaDeEspera();
     }
     
     
+    public int getId(){return this.id;}
+    public String getMarca() {return this.marca;}
+    public String getModelo(){return this.modelo;}
     public double getVelocidadeMedia()               { return this.velocidadeMedia;     }
     public double getPrecoBase()                     { return this.precoBase;           }
     public int  getFactorDeFiabilidade()             { return this.factorDeFiabilidade; }
     public int getCapacidade()                       { return this.capacidade;          }
-    public ArrayList<Viagem> getHistoricoDeViagens() { return this.historicoDeViagens;  }
+    public double gettotalKms(){return this.totalKms;}
+    public List<Viagem> getHistoricoDeViagens()      { return this.historicoDeViagens;  }
+    public List<Viagem> getFilaDeEspera()            { return this.filaDeEspera;        }
     
+    public void setId(int id1) {this.id=id1;}
+    public void setMarca(String marca1) {this.marca = marca1;}
+    public void setModelo(String modelo1) {this.modelo = modelo1;}
+    public void setVelocidadeMedia(double v) {this.velocidadeMedia = v;}
+    public void setPrecoBase(double p) {this.precoBase = p;}
+    public void setFactorFiabilidade(int f) {this.factorDeFiabilidade = f;}
+    public void setCapacidade(int c) {this.capacidade = c;}
+    public void settotalKms(double k){this.totalKms=k;}
+    public void setHistoricoDeViagems(List<Viagem> l) {this.historicoDeViagens = l;}
+    public void setFilaDeEspera(List<Viagem> fe)      { this.filaDeEspera = fe;    }
     
     public boolean equals(Object o)
     {
@@ -64,26 +100,44 @@ public abstract class Veiculo
         }
             
         Veiculo v =(Veiculo) o;  
-        return this.velocidadeMedia == v.getVelocidadeMedia()
+        return this.id== v.getId()
+            && this.marca.equals(v.getMarca())
+            && this.modelo.equals(v.getModelo())
+            && this.velocidadeMedia == v.getVelocidadeMedia()
             && this.precoBase == v.getPrecoBase()
             && this.factorDeFiabilidade == v.getFactorDeFiabilidade()
             && this.capacidade == v.getCapacidade()
-            && this.historicoDeViagens.equals(v.getHistoricoDeViagens());
+            && this.historicoDeViagens.equals(v.getHistoricoDeViagens())
+            && this.filaDeEspera.equals(v.getFilaDeEspera());
     }
     
     
     public String toString()
     {
         StringBuilder string = new StringBuilder();
-        string.append("VelocidadeMedia: "); string.append(this.velocidadeMedia);
-        string.append("PrecoBase: "); string.append(this.precoBase);
-        string.append("FactorDeFiabilidade: "); string.append(this.factorDeFiabilidade);
-        string.append("Capacidade: "); string.append(this.capacidade);
-        string.append("HistoricoDeViagens: "); string.append(this.historicoDeViagens.toString());
+        string.append("ID do taxi                          |   "); string.append(this.id); string.append("\n");
+        string.append("Marca do automóvel                  |   "); string.append(this.marca); string.append("\n");
+        string.append("Modelo do automóvel                 |   "); string.append(this.modelo); string.append("\n");
+        string.append("Velocidade média                    |   "); string.append(this.velocidadeMedia); string.append("\n");
+        string.append("Preco base                          |   "); string.append(this.precoBase); string.append("\n");
+        string.append("Factor de fiabilidade               |   "); string.append(this.factorDeFiabilidade); string.append("\n");
+        string.append("Capacidade                          |   "); string.append(this.capacidade); string.append("\n");
+        string.append("Histórico de viagens                |   "); string.append(this.historicoDeViagens.toString()); string.append("\n");
+        string.append("Fila de espera                      |   "); string.append(this.filaDeEspera.toString()); string.append("\n");
         
         return string.toString();
     }
     
     
     public abstract Veiculo clone();   
+    
+    public void esperaViagem(Viagem v)
+    {
+        this.filaDeEspera.add(v);
+    }
+    
+    public void removeViagem()
+    {
+        this.filaDeEspera.remove(0);
+    }
 }
